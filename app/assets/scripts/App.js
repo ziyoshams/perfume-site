fb9b44bc0d0ea3e3ce12b66ae0899bb4f097897e
Ziyovuddin <z.shamsiddini@gmail.com>
2017-03-27 06:19:55 -0400

    Added animation on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,7 @@
 import MobileMenu from './modules/MobileMenu.js';
 import RevealOnScroll from './modules/RevealOnScroll.js';
-
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
-var revealOnScroll = new RevealOnScroll();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "70%");
--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,17 +1,31 @@
 import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
 class RevealOnScroll{
-  constuctor (){
-    console.log('fucking hell');
-    this.itemsToReveal = $(".feature-item");
+  constructor(elems, offset){
+    this.itemsToReveal = elems;
+    this.offsetPercentage = offset;
     this.hideInitially();
+    this.createWaypoints();
   }
 
   hideInitially(){
     this.itemsToReveal.addClass("reveal-item");
-    alert('working');
   }
 
+  createWaypoints(){
+    var that = this; // to save the object
+    this.itemsToReveal.each(function(){
+      var currentItem = this;
+      new Waypoint({
+        element: currentItem,
+        handler: function(){
+          $(currentItem).addClass('reveal-item--is-visible');
+        },
+        offset: that.offsetPercentage
+      });
+    });
+  }
 
 }
 
