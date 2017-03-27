51ea5e3ca002fc6998dfc97fc07de93400e24d34
Ziyovuddin <z.shamsiddini@gmail.com>
2017-03-27 07:13:44 -0400

    Sticky header is implemented and smooth scroll is working

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import MobileMenu from './modules/MobileMenu.js';
 import RevealOnScroll from './modules/RevealOnScroll.js';
+import StickyHeader from './modules/StickyHeader.js';
 import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+var sticlyHeader = new StickyHeader();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "70%");
